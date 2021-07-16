4565571af442774fa65d8973df559d469ed8c8c9
cvam77 <schaudhary@patriots.uttyler.edu>
2021-07-16 17:03:38 -0500

    experimenting with three js

--- Website/main.js
+++ Website/main.js
@@ -1,194 +1,111 @@
-import gsap from "https://cdn.skypack.dev/gsap"
-import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js'
-import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js'
-import * as dat from "https://cdn.skypack.dev/dat.gui"
+import './style.css'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const gui = new dat.GUI()
-const world = {
-  plane: {
-    width: 400,
-    height: 400,
-    widthSegments: 50,
-    heightSegments: 50
-  }
-}
-gui.add(world.plane, 'width', 1, 500).onChange(generatePlane)
 
-gui.add(world.plane, 'height', 1, 500).onChange(generatePlane)
-gui.add(world.plane, 'widthSegments', 1, 100).onChange(generatePlane)
-gui.add(world.plane, 'heightSegments', 1, 100).onChange(generatePlane)
 
-function generatePlane() {
-  planeMesh.geometry.dispose()
-  planeMesh.geometry = new THREE.PlaneGeometry(
-    world.plane.width,
-    world.plane.height,
-    world.plane.widthSegments,
-    world.plane.heightSegments
-  )
+const scene = new THREE.Scene();
 
-  // vertice position randomization
-  const { array } = planeMesh.geometry.attributes.position
-  const randomValues = []
-  for (let i = 0; i < array.length; i++) {
-    if (i % 3 === 0) {
-      const x = array[i]
-      const y = array[i + 1]
-      const z = array[i + 2]
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,0.1,1000);
 
-      array[i] = x + (Math.random() - 0.5) * 3
-      array[i + 1] = y + (Math.random() - 0.5) * 3
-      array[i + 2] = z + (Math.random() - 0.5) * 3
-    }
-
-    randomValues.push(Math.random() * Math.PI * 2)
-  }
-
-  planeMesh.geometry.attributes.position.randomValues = randomValues
-  planeMesh.geometry.attributes.position.originalPosition =
-    planeMesh.geometry.attributes.position.array
-
-  const colors = []
-  for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
-    colors.push(0, 0.19, 0.4)
-  }
-
-  planeMesh.geometry.setAttribute(
-    'color',
-    new THREE.BufferAttribute(new Float32Array(colors), 3)
-  )
-}
-
-const raycaster = new THREE.Raycaster()
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(
-  75,
-  innerWidth / innerHeight,
-  0.1,
-  1000
-)
-const renderer = new THREE.WebGLRenderer()
-
-renderer.setSize(innerWidth, innerHeight)
-renderer.setPixelRatio(devicePixelRatio)
-document.body.appendChild(renderer.domElement)
-
-new OrbitControls(camera, renderer.domElement)
-camera.position.z = 50
-
-const planeGeometry = new THREE.PlaneGeometry(
-  world.plane.width,
-  world.plane.height,
-  world.plane.widthSegments,
-  world.plane.heightSegments
-)
-const planeMaterial = new THREE.MeshPhongMaterial({
-  side: THREE.DoubleSide,
-  flatShading: THREE.FlatShading,
-  vertexColors: true
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
 })
-const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
-scene.add(planeMesh)
-generatePlane()
 
-const light = new THREE.DirectionalLight(0xffffff, 1)
-light.position.set(0, -1, 1)
-scene.add(light)
+renderer.setPixelRatio(window.devicePixelRatio);
 
-const backLight = new THREE.DirectionalLight(0xffffff, 1)
-backLight.position.set(0, 0, -1)
-scene.add(backLight)
+renderer.setSize(window.innerWidth,window.innerHeight);
 
-const mouse = {
-  x: undefined,
-  y: undefined
+// camera.position.setZ(30);
+
+renderer.render(scene,camera);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+
+const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const torus = new THREE.Mesh(geometry,material);
+
+// scene.add(torus)
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5,5,5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight,ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200,50);
+// scene.add(lightHelper,gridHelper);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25,24,24);
+  const material = new THREE.MeshStandardMaterial({color: 0xffffff})
+  const star = new THREE.Mesh(geometry,material);
+
+  const [x,y,z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x,y,z);
+  scene.add(star);
 }
 
-let frame = 0
-function animate() {
-  requestAnimationFrame(animate)
-  renderer.render(scene, camera)
-  raycaster.setFromCamera(mouse, camera)
-  frame += 0.01
+Array(200).fill().forEach(addStar);
 
-  const {
-    array,
-    originalPosition,
-    randomValues
-  } = planeMesh.geometry.attributes.position
-  for (let i = 0; i < array.length; i += 3) {
-    // x
-    array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.01
+const spaceTexture = new THREE.TextureLoader().load('./images/space.jpg');
+scene.background = spaceTexture;
 
-    // y
-    array[i + 1] =
-      originalPosition[i + 1] + Math.sin(frame + randomValues[i + 1]) * 0.001
-  }
+const shivamTexture = new THREE.TextureLoader().load('./images/shivam.jpg');
 
-  planeMesh.geometry.attributes.position.needsUpdate = true
+const shivam = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial({ map: shivamTexture })
+);
 
-  const intersects = raycaster.intersectObject(planeMesh)
-  if (intersects.length > 0) {
-    const { color } = intersects[0].object.geometry.attributes
+// scene.add(shivam);
 
-    // vertice 1
-    color.setX(intersects[0].face.a, 0.1)
-    color.setY(intersects[0].face.a, 0.5)
-    color.setZ(intersects[0].face.a, 1)
+const mobileTexture = new THREE.TextureLoader().load('./images/mob.jpg');
 
-    // vertice 2
-    color.setX(intersects[0].face.b, 0.1)
-    color.setY(intersects[0].face.b, 0.5)
-    color.setZ(intersects[0].face.b, 1)
+const mobile = new THREE.Mesh(
+  new THREE.PlaneGeometry(10,10),
+  new THREE.MeshBasicMaterial({ map: mobileTexture })
+);
 
-    // vertice 3
-    color.setX(intersects[0].face.c, 0.1)
-    color.setY(intersects[0].face.c, 0.5)
-    color.setZ(intersects[0].face.c, 1)
 
-    intersects[0].object.geometry.attributes.color.needsUpdate = true
+// mobile.position.z = 50;
+mobile.position.setX(-10);
+mobile.position.setY(-10);
 
-    const initialColor = {
-      r: 0,
-      g: 0.19,
-      b: 0.4
-    }
+// scene.add(mobile)
 
-    const hoverColor = {
-      r: 0.1,
-      g: 0.5,
-      b: 1
-    }
+function moveCamera(){
 
-    gsap.to(hoverColor, {
-      r: initialColor.r,
-      g: initialColor.g,
-      b: initialColor.b,
-      duration: 1,
-      onUpdate: () => {
-        // vertice 1
-        color.setX(intersects[0].face.a, hoverColor.r)
-        color.setY(intersects[0].face.a, hoverColor.g)
-        color.setZ(intersects[0].face.a, hoverColor.b)
+  const t = document.body.getBoundingClientRect().top;
+  mobile.rotation.x += 0.05;
+  mobile.rotation.y += 0.075;
+  mobile.rotation.z += 0.05;
 
-        // vertice 2
-        color.setX(intersects[0].face.b, hoverColor.r)
-        color.setY(intersects[0].face.b, hoverColor.g)
-        color.setZ(intersects[0].face.b, hoverColor.b)
+  shivam.rotation.y += 0.01;
+  shivam.rotation.z += 0.01;
 
-        // vertice 3
-        color.setX(intersects[0].face.c, hoverColor.r)
-        color.setY(intersects[0].face.c, hoverColor.g)
-        color.setZ(intersects[0].face.c, hoverColor.b)
-        color.needsUpdate = true
-      }
-    })
-  }
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera
+
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  // controls.update();
+  
+  renderer.render(scene,camera);
 }
 
 animate()
-
-addEventListener('mousemove', (event) => {
-  mouse.x = (event.clientX / innerWidth) * 2 - 1
-  mouse.y = -(event.clientY / innerHeight) * 2 + 1
-})
